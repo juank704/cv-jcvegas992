c99b23b549d71ccf500fd603b008697edfe2fb2b
Juank704 <juan_k704@hotmail.com>
2020-03-29 16:57:35 -0300

    upload site domain
    upload correct site domain

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://juank704.dev.org`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://juank704.herokuapp.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
