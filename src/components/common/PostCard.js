547ac897f2b04e68bf7a7f8900ef085ba024e655
Juank704 <juan_k704@hotmail.com>
2020-04-15 00:55:24 -0400

    Changes on PostCard CSS and Js added Flag Icon
    Changes on PostCard CSS and Js added Flag Icon

--- src/components/common/PostCard.js
+++ src/components/common/PostCard.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 import { Tags } from '@tryghost/helpers-gatsby'
 import { readingTime as readingTimeHelper } from '@tryghost/helpers'
+import FlagIcon from './FlagIcon'
 
 const PostCard = ({ post }) => {
     const url = `/${post.slug}/`
-    const readingTime = readingTimeHelper(post)
-
+    const jobInfo = JSON.parse(post.codeinjection_foot);
     return (
         <Link to={url} className="post-card">
             <header className="post-card-header">
@@ -20,6 +20,12 @@ const PostCard = ({ post }) => {
                 <h2 className="post-card-title">{post.title}</h2>
             </header>
             <section className="post-card-excerpt">{post.excerpt}</section>
+            <section className="post-card-aside">
+                <div className="post-card-jobInfo">
+                    <span>{jobInfo.startDate} - {jobInfo.endDate}</span>
+                    <FlagIcon {...jobInfo.flag} />
+                </div> 
+            </section>
             <footer className="post-card-footer">
                 <div className="post-card-footer-left">
                     <div className="post-card-avatar">
@@ -31,7 +37,6 @@ const PostCard = ({ post }) => {
                     <span>{ post.primary_author.name }</span>
                 </div>
                 <div className="post-card-footer-right">
-                    <div>{readingTime}</div>
                 </div>
             </footer>
         </Link>
